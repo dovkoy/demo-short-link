d3d5c9da809f59f0b4e97514541e5ae082cfb229
dovid <137943034+dovkoy@users.noreply.github.com>
2025-07-17 16:49:11 -0400

    Implement redirect

--- nextjs/app/[alias]/page.tsx
+++ nextjs/app/[alias]/page.tsx
@@ -1,6 +1,10 @@
 // BH
 
 import { redirect } from "next/navigation";
+import { db } from "@/lib/db/drizzle";
+import { eq } from 'drizzle-orm';
+import { shortLinks } from "@/lib/db/schema";
+import { notFound } from "next/navigation";
 
 export default async function Page({
   params,
@@ -9,13 +13,18 @@ export default async function Page({
 }) {
   
   // fetch record
-  const { alias } = await params
-  const destinationUrl = 'https://www.google.com/'
-  
-  // tbd analytics
+  const { alias } = await params;
+  const [ result ] = await db.select({
+      destinationUrl: shortLinks.longLink
+    })
+    .from(shortLinks)
+    .where(eq(shortLinks.alias, alias))
+    .limit(1);
 
-
-  return (
-    redirect(destinationUrl)
-  );
+  if (result) {
+    // tbd analytics
+    return redirect(result.destinationUrl)
+  } else {
+    notFound()
+  }
 }
